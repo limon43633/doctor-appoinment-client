ee44d72f2d3935f67dcb458bcc6af6805f7b72f0
hassibur rahman <hassiburrahman@hassiburs-Mac-mini.local>
2026-08-07 23:14:23 +0600

    working in appoinment page

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -9,16 +9,60 @@ const Appointment = () => {
   const {doctors, currencySymbol} = useContext(AppContext)
 
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
-    console.log(docInfo)
+  }
+
+  const getAvailableSlots = async () => {
+    setDocSlots([])
+
+    // getting current date
+    let today = new Date()
+
+    for(let i = 0 ; i < 7; i++){
+      // getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+1)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while(currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hours: '2-digit', minute: '2-digit'})
+
+        // add slot to array
+        timeSlots
+      }
+
+    }
   }
 
   useEffect(()=>{
     fetchDocInfo()
   },[doctors, docId])
+
+
+  useEffect(()=>{
+    getAvailableSlots
+  },[docInfo])
 
 
   return docInfo && (
@@ -50,6 +94,10 @@ const Appointment = () => {
          </div>
 
       </div>
+
+    {/**/}
+
+
     </div>
   )
 }
